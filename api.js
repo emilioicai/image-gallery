0d23c24462fc9f96d0972d5cf05aa29e718fb348
Emilio Rodriguez <emiliorodriguez@gmail.com>
2017-03-16 16:29:18 +0100

    initial api implementation

---
+++ api.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const request = require('request');
+
+exports.getImagesFromPage = function(req, res) {
+	request(
+		`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=f7a59cbf8bf9d30665687c9163fd36a3&tags=paris&per_page=10&page=${req.params.page}&format=json&jsoncallback=parseJson&extras=owner_name,url_m`,
+		function (error, response, body) {
+			const parseJson = (obj) => obj && obj.photos && obj.photos.photo;
+			let photos = [];
+
+			if (!error && response.statusCode == 200) {
+				photos = eval(body);
+			}
+
+			res.json(photos);
+		}
+	);
+};
--- server.js
+++ server.js
@@ -2,21 +2,14 @@ const express = require('express');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpack = require('webpack');
 const webpackConfig = require('./webpack.config.js');
+const api = require('./api');
 const app = express();
  
 const compiler = webpack(webpackConfig);
  
 app.use(express.static(__dirname + '/www'));
  
-app.use(webpackDevMiddleware(compiler, {
-  hot: true,
-  filename: 'bundle.js',
-  publicPath: '/',
-  stats: {
-    colors: true,
-  },
-  historyApiFallback: true,
-}));
+app.get('/images/:page', api.getImagesFromPage);
  
 const server = app.listen(3000, function() {
   const host = server.address().address;
